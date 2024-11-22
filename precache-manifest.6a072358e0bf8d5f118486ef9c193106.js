8fd7a6fa3423a5bd8dc88a406b8f41076b86469f
mauriciogonzalezvalero <mauricio.gonzalezvalero@udem.edu>
2024-11-22 16:19:33 -0600

    Updates

---
+++ precache-manifest.6a072358e0bf8d5f118486ef9c193106.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9845f422dc840d55751bd7c5936a407",
+    "url": "/PortfolioProject-NotesApp/index.html"
+  },
+  {
+    "revision": "5881dc019c8507c67516",
+    "url": "/PortfolioProject-NotesApp/static/css/main.5ca1cbe0.chunk.css"
+  },
+  {
+    "revision": "51bd67a846cf151bf8e6",
+    "url": "/PortfolioProject-NotesApp/static/js/2.80978b76.chunk.js"
+  },
+  {
+    "revision": "5881dc019c8507c67516",
+    "url": "/PortfolioProject-NotesApp/static/js/main.53f4dcaa.chunk.js"
+  },
+  {
+    "revision": "b3c4906d8dfa6625c1e4",
+    "url": "/PortfolioProject-NotesApp/static/js/runtime-main.5d246243.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/PortfolioProject-NotesApp/precache-manifest.6a072358e0bf8d5f118486ef9c193106.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PortfolioProject-NotesApp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
